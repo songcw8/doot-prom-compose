9cd6766cd443481cd2117593d77b3fce3d9744f1
songcw8 <ben990158@gmail.com>
2025-05-27 10:23:40 +0900

    Initial commit

---
+++ src/main/java/org/example/dootpromcompose/DootPromComposeApplication.java
@@ -0,0 +1,13 @@
+package org.example.dootpromcompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DootPromComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DootPromComposeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dootpromcompose/DootPromComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dootpromcompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DootPromComposeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
