68dade6a02460d9a21551e11dc8e4100bc22a6b5
songcw8 <ben990158@gmail.com>
2025-05-27 10:47:30 +0900

    의존성 코어 및 config

---
+++ src/main/java/org/example/dootpromcompose/config/MetricsConfig.java
@@ -0,0 +1,14 @@
+package org.example.dootpromcompose.config;
+
+import io.micrometer.core.aop.CountedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+    @Bean
+    public CountedAspect countedAspect(MeterRegistry registry) {
+        return new CountedAspect(registry);
+    }
+}
